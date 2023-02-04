f28a847278e37c1c6c283ed89b6f1c24070e0bf0
MD OWAIS ATHAR <owais.athar.8092@gmail.com>
2023-02-04 15:05:57 +0530

    Slideshow banner

--- src/Components/Slideshow.jsx
+++ src/Components/Slideshow.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Slideshow = () => {
-  return (
-    <div>Slideshow</div>
-  )
+    const [index, setIndex] = useState(0);
+    const slides = [
+        {
+            image: "https://i.ibb.co/syLR9tf/Image1.png",
+            alt: "Image 1"
+        },
+        {
+            image: "https://i.ibb.co/3vJ48FF/Image2.png",
+            alt: "Image 2"
+        },
+        {
+            image: "https://i.ibb.co/FD9HKZH/Image3.png",
+            alt: "Image 3"
+        }
+    ];
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setIndex(prevIndex => (prevIndex + 1) % slides.length)
+        }, 2000);
+        return () => clearInterval(intervalId);
+    }, [slides.length])
+
+    return (
+        <img
+            src={slides[index].image}
+            alt={slides[index].alt}
+            className=" rounded-lg  flex h-screen w-full items-center justify-center"
+        />
+    )
 }
 
 export default Slideshow
